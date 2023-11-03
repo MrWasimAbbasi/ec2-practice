ab52bc0327610a0bfdd8a5088f7c92c852055e84
MWaseemAbbasi <mwaseem@tekambi.com>
2023-11-04 01:48:02 +0500

    datatables added

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- Include DataTables CSS from the DataTables CDN -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+
     <title>Deploy to AWS Instance!!</title>
 </head>
 <body>
 <h1>Data Display</h1>
-<table>
+<table class="table table-bordered table-hover table-stripped">
     <tr>
         <th>ID</th>
         <th>Name </th>
